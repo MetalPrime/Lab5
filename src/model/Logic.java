a0c0c0c2e513344213cc4825e8993e9d88041049
Jhon Garcia <metalprime57@gmail.com>
2020-04-03 02:49:26 -0500

    changes txt

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,5 +1,29 @@
 package model;
 
-public class Logic {
+import java.util.LinkedList;
 
+public class Logic {
+	
+	private LinkedList<Dog>DogList;
+	private LinkedList<Cat>CatList;
+	
+	public Logic() {
+		DogList = new LinkedList<Dog>();
+		CatList = new LinkedList<Cat>();
+	}
+
+	/**
+	 * @return the dogList
+	 */
+	public LinkedList<Dog> getDogList() {
+		return DogList;
+	}
+
+	/**
+	 * @return the catList
+	 */
+	public LinkedList<Cat> getCatList() {
+		return CatList;
+	}
+	
 }
